f590e39160a2582f05bf3fcb599839e0df209725
bill <billcreswell@gmail.om>
2013-01-25 21:13:20 -0500

    Some code formatting, remove home link from home page

--- app/excite.php
+++ app/excite.php
@@ -2,164 +2,240 @@
 /* Excite WebSite Manager
  * author: Bill Creswell
  * license: use it or not
- * 
+ *
  * Goal: A flat file site that can be easily maintained by user
- * Idea: 
+ * Idea:
  * -A template with fixed main top-level pages
  * -pages, events, news and newsletters will allow user drop in text files
  * Required files
- * 
 */
 
-#get requested page
-if(isset($_REQUEST["page"]) && $_REQUEST["page"] != "") {
-  $page=$_REQUEST["page"];
-} else $page="index";
+// get requested page
+    if (isset($_REQUEST["page"]) && $_REQUEST["page"] != "") {
+        $page = $_REQUEST["page"];
+    } else $page = "home";
 
 /**
  * Get Content
- * this will get the top level content
+ * @description: basic routing - this will get the top level content
  */
- 
-function getContent($page) {
+    function getContent($page)
+    {
+        switch ($page) {
+            case "home":
+            case "index":
+            case "":
+            // home page
+                $content = file_get_contents("pages/home.html");
+                break;
+            case "newsletters":
+            // newsletter page
+                $content = file_get_contents("pages/newsletters.html");
+            // add director list
+                $content .= getDirList($page);
+                break;
 
- switch ($page) {
-  case "index":
-    #content="Index";
-    $content = file_get_contents("pages/index.html");
-  break;
-  
-  case "newsletters":
-    $content = file_get_contents("pages/newsletters.html");
-    $content.= getDirList($page);
-  break;
-  
-  default:
-    $content = file_get_contents("pages/$page.html");
-  break; 
- }   
- return $content;
-}
+            default:
+                $content = file_get_contents("pages/$page.html");
+                break;
+        }
+        return $content;
+    }
 
-function getMenuList() {
- $menu="";
- $dirlist = getFileList("pages/", true, 1); 
- foreach($dirlist as $file) { 
-  $menu.="<li><a href='?page={$file['call']}'>{$file['name']}</a></li>"; 
- };
-return $menu;
-}
+/**
+ * Menu List
+ * @description Get menu from list of files in pages directory
+ * @return html list of pages for menu
+ */
+    function getMenuList()
+    {
+    // show home menu first, everywhere but the home page
+        if (isset($_REQUEST["page"]) && $_REQUEST["page"] != "home") {
+            $menu = "<li><a href='/'>Home</a></li>";
+        } else {
+            $menu = "";
+        }
+    // get listing from the pages directory
+        $dirlist = getFileList("pages/", true, 1);
+        foreach ($dirlist as $file) {
+        // show all items in pages except home
+            if ($file['name'] != "Home") {
+                $menu.="<li><a href='?page={$file['call']}'>{$file['name']}</a></li>";
+            }
+        };
+    // additional menu items
+        $menu .= "<li><a href='https://www.facebook.com/pages/Michigan-Council-Of-The-Blind-Visually-Impaired/125509287540911'>MCBVI on Facebook</a></li>";
+        return $menu;
+    }
 
-function getDirList($dir) {
- $list="";
- $dirlist = getFileList("$dir/", true, 1); 
- foreach($dirlist as $file) { 
-  $list.="<li>{$file['name']}</li>"; 
- };
-return $list;
-}
+/**
+ * Get Directory List
+ * @param string directory
+ * @return html list of links
+ */
+    function getDirList($dir)
+    {
+        $list = "";
+        $dirlist = getFileList("$dir/", true, 1);
+        foreach ($dirlist as $file) {
+            $fname = $file["path"];
+            $list .= "<li><a href='$fname'>{$file['name']} ({$file['type']})</li>";
+        };
+        return $list;
+    }
 
-
-// single directory 
-//$dirlist = getFileList("./"); 
-// include all subdirectories recursively 
-//$dirlist = getFileList("./documents/", true); 
-// include just one or two levels of subdirectories 
-//$dirlist = getFileList("./documents/", true, 1); 
+//********************
+// Using the getFile List
+// single directory
+//$dirlist = getFileList("./");
+// include all subdirectories recursively
+//$dirlist = getFileList("./documents/", true);
+// include just one or two levels of subdirectories
+//$dirlist = getFileList("./documents/", true, 1);
 //$dirlist = getFileList("./documents/", true, 2);
- 
-// Original PHP code by Chirp Internet: www.chirp.com.au 
-// Please acknowledge use of this code by including this header. 
 
-function getFileList($dir, $recurse=false, $depth=false) { 
-$types = array(
-"doc" => "word_icon.png", 
-"gif" => "image_icon.png", 
-"jpeg" => "image_icon.png", 
-"jpg" => "image_icon.png", 
-"html" => "ie_icon.png", 
-"txt" => "document_icon.png", 
-"pdf" => "pdf_icon.png", 
-"ppt" => "powerpoint_icon.png", 
-"xls" => "excel_icon.png"
-);
- 
-  $retval = array(); 
-// add trailing slash if missing 
-  if(substr($dir, -1) != "/") $dir .= "/"; 
-// open pointer to directory and read list of files 
-  $d = @dir($dir) or die("getFileList: Failed opening directory $dir for reading"); 
-  while(false !== ($entry = $d->read())) { 
-  // skip hidden files 
-    if($entry[0] == ".") continue; 
-    if(is_dir("$dir$entry")) { 
-      $retval[] = array( 
-        "path" => "$dir$entry/", 
-        "name" => "$entry", 
-        "type" => filetype("$dir$entry"), 
-        "size" => 0, 
-        "lastmod" => filemtime("$dir$entry")
-      ); 
-      if($recurse && is_readable("$dir$entry/")) { 
-        if($depth === false) { 
-          $retval = array_merge(
-          $retval, getFileList("$dir$entry/", true)); 
-        } elseif($depth > 0) { 
-          $retval = array_merge(
-            $retval, getFileList("$dir$entry/", true, $depth-1)); 
-        } 
-      } 
-    } elseif(is_readable("$dir$entry")) { 
-      $ic = explode(".", "$entry");
-      $type = $ic[1];
-      $name = ucwords(str_replace("_"," ",$ic[0]));
-      $call = $ic[0];
-      $icon = $types[$ic[1]];
-      $retval[] = array( 
-        "path" => "$dir$entry",
-        "call" => "$call",
-        "name" => "$name", 
-        "type" => "$ic[1]", 
-        "icon" => "$icon", 
-        "size" => filesize("$dir$entry"), 
-        "lastmod" => filemtime("$dir$entry") ); 
-    } 
-  } $d->close(); return $retval; } 
+// Original PHP code by Chirp Internet: www.chirp.com.au
+// Please acknowledge use of this code by including this header.
+/***************************************************************/
+ /**
+  * Get File List
+  * @description directory listing
+  * @param string dir, boolean recurse, int depth
+  * @return retval array of files
+  */
+    function getFileList($dir, $recurse = false, $depth = false)
+    {
+        $retval = array();
+    // incase you want to link with icons
+        $types = array(
+            "doc" => "word_icon.png",
+            "gif" => "image_icon.png",
+            "jpeg" => "image_icon.png",
+            "jpg" => "image_icon.png",
+            "html" => "ie_icon.png",
+            "txt" => "document_icon.png",
+            "pdf" => "pdf_icon.png",
+            "ppt" => "powerpoint_icon.png",
+            "xls" => "excel_icon.png"
+        );
+    // add trailing slash if missing
+        if (substr($dir, -1) != "/") {
+            $dir .= "/";
+        }
+    // open pointer to directory and read list of files
+        $d = @dir($dir) or die("getFileList: Failed opening directory $dir for reading");
+
+        while (false !== ($entry = $d->read())) {
+        // skip hidden files
+            if ($entry[0] == ".") {
+                continue;
+            }
+        // is directory
+            if (is_dir("$dir$entry")) {
+                $retval[] = array(
+                    "path" => "$dir$entry/",
+                    "name" => "$entry",
+                    "type" => filetype("$dir$entry"),
+                    "size" => 0,
+                    "lastmod" => filemtime("$dir$entry")
+                );
+            // check directory for recursion
+                if ($recurse && is_readable("$dir$entry/")) {
+                    if ($depth === false) {
+                        $retval = array_merge(
+                        $retval, getFileList("$dir$entry/", true));
+                    } elseif ($depth > 0) {
+                        $retval = array_merge(
+                        $retval, getFileList("$dir$entry/", true, $depth-1));
+                    }
+                }
+            } elseif (is_readable("$dir$entry")) {
+                $ic = explode(".", "$entry");
+                $type = $ic[1];
+                $name = ucwords(str_replace("_"," ",$ic[0]));
+                $call = $ic[0];
+                $icon = $types[$ic[1]];
+                $retval[] = array(
+                    "path" => "$dir$entry",
+                    "call" => "$call",
+                    "name" => "$name",
+                    "type" => "$ic[1]",
+                    "icon" => "$icon",
+                    "size" => filesize("$dir$entry"),
+                    "lastmod" => filemtime("$dir$entry") );
+            }
+        }
+        $d->close();
+        return $retval;
+    }
+
+// Template
 
 ?><!doctype html>
-<htm>
-<head>
-<title><?php echo $page; ?></title>
-<link href="app/miblind.css" rel="stylesheet" type="text/css" />
+<html>
+
+    <head>
+
+    <title><?php echo $page; ?></title>
+
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+    <meta charset="UTF-8"/>
+
+    <meta name="HandheldFriendly" content="true"/><!--Blackberry Column View-->
+    <meta name="viewport" content="initial-scale=1.0"/><!--iPod-->
+    <meta name="viewport" content="width=device-width"/><!--android-->
+    <link rel="stylesheet" href="app/mobile.css" type="text/css" media="only screen and (max-width : 39em)"/>
+    <!--[if lt IE 9]>
+    <link rel="stylesheet" media="all" type="text/css" href="app/miblind.css"/>
+    <![endif]-->
+    <link rel="stylesheet" href="app/miblind.css" type="text/css" media="only screen and (min-width : 39em)"/>
+
+    <!--<link href="images/logo57.png" rel="apple-touch-icon"/>-->
+
+    <script type="text/javascript">
+    //<![CDATA[
+    // Google Analytics
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-31081251-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    //]]>
+    </script>
+
 </head>
+
 <body>
 
-<div id="Banner">
-  
-<div>Blind and Visually Impaired 
-  <a href="#Menu">Menu</a>
-  <a href="#Content">Content</a>
-  <form style="display:inline;float:right;" action="search.php" method="GET">
-      <label for="SiteSearch">Search</label>
-      <input type="search" name="keyword" id="SiteSearch" placeholder="search" />
-      <input type="submit" name="action" value="SEARCH" />
-      <input type="hidden" name="limit" value="10" />
-  </form> 
-  <br style="clear:both"/>
-</div>
+<?php
+// display banner on anything but home page
+    if(isset($_REQUEST["page"]) && ($_REQUEST["page"] != "home")) {
+?>
 
-</div>
+    <div id="Banner">
+        <a href="/">MCBVI</a>
+        <a href="#Menu">Skip to Menu</a>
+        <br style="clear:both"/>
+    </div>
 
-<div id="Content">
+<?php }
+// Display Page Content
+?>
+    <div id="Content">
+        <?php echo getContent($page); ?>
+    </div>
+<?php
+//Display menu
+?>
+    <div id = "Menu" role = "nav">
+        <?php echo getMenuList(); ?>
+    </div>
 
-<div id="Main">
-<?php echo getContent($page); ?>
-</div>
-
-<div id="Menu" role="nav">
-<?php echo getMenuList(); ?>
-</div>
-
-</div>
 </body>
 </html>
--- search.php
+++ search.php
@@ -2,200 +2,195 @@
 
 /* terraserver.de/search-0.2-11.04.2002 - http://www.terraserver.de/*/
 
-
 $my_server = "http://".getenv("SERVER_NAME");
-$my_webroot = "/code/excitecms/";
-$my_root = getenv("DOCUMENT_ROOT") . "/code/excitecms/"; 
-$s_dirs = array("pages","newsletters","events"); 
-$s_skip = array("..",".","subdir2"); 
-$s_files = "html|htm|HTM|HTML|php|txt|doc"; 
-$min_chars = "3"; 
-$max_chars = "30"; 
-$default_val = "Searchphrase"; 
-$limit_hits = array("5","10","25","50","100"); 
-$message_1 = "Invalid Searchterm!"; 
+$my_webroot = "/filecms/";
+$my_root = getenv("DOCUMENT_ROOT") . "/filecmsc/";
+
+$s_dirs = array("pages","newsletters","events");
+$s_skip = array("..",".","subdir2");
+$s_files = "html|htm|HTM|HTML|php|txt|doc";
+
+$min_chars = "3";
+$max_chars = "30";
+
+$default_val = "Searchphrase";
+
+$limit_hits = array("5","10","25","50","100");
+
+$message_1 = "Invalid Searchterm!";
 $message_2 = "Please enter at least '$min_chars', highest '$max_chars' characters.";
-$message_3= "Your searchresult for:"; 
-$message_4 = "Sorry, no hits."; 
-$message_5 = "results"; 
-$message_6 = "Match case"; 
-$no_title = "Untitled"; 
-$limit_extracts_extracts = ""; 
-$byte_size = "51200"; 
+$message_3= "Your searchresult for:";
+$message_4 = "Sorry, no hits.";
+$message_5 = "results";
+$message_6 = "Match case";
 
-# render search_form(): 
-function search_form($HTTP_GET_VARS, $limit_hits, $default_val, $message_5, $message_6, $PHP_SELF) {
-	@$keyword=$HTTP_GET_VARS['keyword'];
-	@$case=$HTTP_GET_VARS['case'];
-	@$limit=$HTTP_GET_VARS['limit'];
-	echo
-	"<form action=\"$PHP_SELF\" method=\"GET\">\n",
-	"<input type=\"hidden\" value=\"SEARCH\" name=\"action\">\n",
-	"<input type=\"text\" name=\"keyword\" class=\"text\" size=\"10\"  maxlength=\"30\" value=\"";
-	if(!$keyword)
-		echo "$default_val";
-	else
-		echo str_replace("&amp;","&",htmlentities($keyword));
-	echo "\" ";
-	echo "onFocus=\" if (value == '";
-	if(!$keyword)
-		echo "$default_val"; 
-	else
-		echo str_replace("&amp;","&",htmlentities($keyword));
-	echo "') {value=''}\" onBlur=\"if (value == '') {value='";
-	if(!$keyword)
-		echo "$default_val"; 
-	else
-		echo str_replace("&amp;","&",htmlentities($keyword));
-	echo "'}\"> ";
-	$j=count($limit_hits);
-	if($j==1)
-		echo "<input type=\"hidden\" value=\"".$limit_hits[0]."\" name=\"limit\">";
-	elseif($j>1) {
-		echo
-		"<select name=\"limit\" class=\"select\">\n";
-		for($i=0;$i<$j;$i++) {
-			echo "<option value=\"".$limit_hits[$i]."\"";
-			if($limit==$limit_hits[$i])
-				echo "SELECTED";
-			echo ">".$limit_hits[$i]." $message_5</option>\n";
-			}
-		echo "</select> ";
-		}
-	echo
-	"<input type=\"submit\" value=\"OK\" class=\"button\">\n",
-	"<br>\n",
-	"<span class=\"checkbox\">$message_6</span> <input type=\"checkbox\" name=\"case\" value=\"true\" class=\"checkbox\"";
-	if($case)
-		echo " CHECKED";
-	echo
-	">\n",
-	"<br>\n",
-	"<a href=\"http://www.terraserver.de/\" class=\"ts\" target=\"_blank\">Powered by terraserver.de/search</a>",
-	"</form>\n";
-	}
+$no_title = "Untitled";
+$limit_extracts_extracts = "";
+$byte_size = "51200";
 
+// render search_form():
+    function search_form($HTTP_GET_VARS, $limit_hits, $default_val, $message_5, $message_6, $PHP_SELF)
+    {
+        @$keyword = $HTTP_GET_VARS['keyword'];
+        @$case = $HTTP_GET_VARS['case'];
+        @$limit = $HTTP_GET_VARS['limit'];
+        echo
+        "<form action=\"$PHP_SELF\" method=\"GET\">\n",
+        "<input type=\"hidden\" value=\"SEARCH\" name=\"action\">\n",
+        "<input type=\"text\" name=\"keyword\" class=\"text\" size=\"10\"  maxlength=\"30\" value=\"";
+        if (!$keyword) echo "$default_val";
+        else echo str_replace("&amp;","&",htmlentities($keyword));
+        echo "\" ";
+        echo "onFocus=\" if (value == '";
+        if (!$keyword) echo "$default_val";
+        else echo str_replace("&amp;","&",htmlentities($keyword));
+        echo "') {value=''}\" onBlur=\"if (value == '') {value='";
+        if (!$keyword) echo "$default_val";
+        else echo str_replace("&amp;","&",htmlentities($keyword));
+        echo "'}\"> ";
+        $j = count($limit_hits);
+        if ($j == 1) echo "<input type=\"hidden\" value=\"".$limit_hits[0]."\" name=\"limit\">";
+        elseif ($j>1) {
+            echo
+            "<select name=\"limit\" class=\"select\">\n";
+            for ($i=0;$i<$j;$i++) {
+                echo "<option value=\"".$limit_hits[$i]."\"";
+                if($limit==$limit_hits[$i]) echo "SELECTED";
+                echo ">".$limit_hits[$i]." $message_5</option>\n";
+            }
+            echo "</select> ";
+        }
+        echo
+            "<input type=\"submit\" value=\"OK\" class=\"button\">\n",
+            "<br>\n",
+            "<span class=\"checkbox\">$message_6</span>
+            <input type=\"checkbox\" name=\"case\" value=\"true\" class=\"checkbox\"";
+            if ($case)  echo " CHECKED";
+            echo ">\n",
+            "<br>\n",
+            "<a href=\"http://www.terraserver.de/\" class=\"ts\" target=\"_blank\">Powered by terraserver.de/search</a>",
+            "</form>\n";
+    }
 
-# search_headline(): 
-function search_headline($HTTP_GET_VARS, $message_3) {
-	@$keyword=$HTTP_GET_VARS['keyword'];
-	@$action=$HTTP_GET_VARS['action'];
-	if($action == "SEARCH") // Volltextsuche
-		echo "<h1 class=\"result\">$message_3 '<i>".htmlentities(stripslashes($keyword))."</i>'</h1>";
-	}
+    # search_headline():
+    function search_headline($HTTP_GET_VARS, $message_3) {
+        @$keyword = $HTTP_GET_VARS['keyword'];
+        @$action = $HTTP_GET_VARS['action'];
+        if ($action == "SEARCH") // Volltextsuche
+        echo "<h1 class=\"result\">$message_3 '<i>".htmlentities(stripslashes($keyword))."</i>'</h1>";
+    }
 
-# search_error():
-function search_error($HTTP_GET_VARS, $min_chars, $max_chars, $message_1, $message_2, $limit_hits) {
-	global $HTTP_GET_VARS;
-	@$keyword=$HTTP_GET_VARS['keyword'];
-	@$action=$HTTP_GET_VARS['action'];
-	@$limit=$HTTP_GET_VARS['limit'];
-	if($action == "SEARCH") { 
-		if(strlen($keyword)<$min_chars||strlen($keyword)>$max_chars||!in_array ($limit, $limit_hits)) { 
-			echo "<p class=\"result\"><b>$message_1</b><br>$message_2</p>";
-			$HTTP_GET_VARS['action'] = "ERROR"; 
-			}
-		}
-	}
+    # search_error():
+    function search_error($HTTP_GET_VARS, $min_chars, $max_chars, $message_1, $message_2, $limit_hits) {
+        global $HTTP_GET_VARS;
+        @$keyword=$HTTP_GET_VARS['keyword'];
+        @$action=$HTTP_GET_VARS['action'];
+        @$limit=$HTTP_GET_VARS['limit'];
+        if ($action == "SEARCH") {
+            if (strlen($keyword)<$min_chars||strlen($keyword)>$max_chars||!in_array ($limit, $limit_hits)) {
+                echo "<p class=\"result\"><b>$message_1</b><br>$message_2</p>";
+                $HTTP_GET_VARS['action'] = "ERROR";
+            }
+        }
+    }
 
-# search_dir(): 
-function search_dir($my_server, $my_root, $s_dirs, $s_files, $s_skip, $message_1, $message_2, $no_title, $limit_extracts, $byte_size, $HTTP_GET_VARS) {
+    # search_dir():
+    function search_dir($my_server, $my_root, $s_dirs, $s_files, $s_skip, $message_1, $message_2, $no_title, $limit_extracts, $byte_size, $HTTP_GET_VARS) {
+        global $count_hits;
+        @$keyword = $HTTP_GET_VARS['keyword'];
+        @$action = $HTTP_GET_VARS['action'];
+        @$limit = $HTTP_GET_VARS['limit'];
+        @$case = $HTTP_GET_VARS['case'];
 
-  global $count_hits;
-  
-	@$keyword = $HTTP_GET_VARS['keyword'];
-	@$action = $HTTP_GET_VARS['action'];
-	@$limit = $HTTP_GET_VARS['limit'];
-	@$case = $HTTP_GET_VARS['case'];
-
-	if($action == "SEARCH") { 
-		foreach($s_dirs as $dir) { 
-
-      $handle = @opendir($my_root.$dir);
-      
-			while ($file = @readdir($handle)) {
-				if (in_array($file, $s_skip)) { continue; }
-				elseif ($count_hits >= $limit) { break; }
-				elseif(is_dir($my_root.$dir."/".$file)) { 
-					$s_dirs = array("$dir/$file");
-					search_dir($my_server, $my_root, $s_dirs, $s_files, $s_skip, $message_1, $message_2, $no_title, $limit_extracts, $byte_size, $HTTP_GET_VARS); // search_dir() rekursiv auf alle Unterverzeichnisse aufrufen
-					}
-				elseif(preg_match("/($s_files)$/i", $file)) { // Alle Dateien gemaess Endungen $s_files
-					$fd=fopen($my_root.$dir."/".$file,"r");
-					$text=fread($fd, $byte_size); // 50 KB
-					$keyword_html = htmlentities($keyword);
-					if($case) { // Gross-/Kleinschreibung beruecksichtigen?
-						$do=strstr($text, $keyword)||strstr($text, $keyword_html);
-						}
-					else {
-						$do=stristr($text, $keyword)||stristr($text, $keyword_html);
-						}
-					if($do)	{
-						$count_hits++; // Treffer zaehlen
-						if(preg_match_all("=<title[^>]*>(.*)</title>=siU", $text, $titel)) { // Generierung des Link-Textets aus <title>...</title>
-							if(!$titel[1][0]) // <title></title> ist leer...
-								$link_title=$no_title; // ...also $no_title
-							else
-								$link_title=$titel[1][0];  // <title>...</title> vorhanden...
-							}
-						else {
-							$link_title=$no_title; // ...ansonsten $no_title
-							}
-            $page=explode(".",$file);  
-						echo "<a href=\"/code/excitecms?page=$page[0]\" target=\"_self\" class=\"result\">$count_hits.  $link_title</a><br>"; // Ausgabe des Links
-						$auszug = strip_tags($text);
-						$keyword = preg_quote($keyword); // unescapen
-						$keyword = str_replace("/","\/","$keyword");
-						$keyword_html = preg_quote($keyword_html); // unescapen
-						$keyword_html = str_replace("/","\/","$keyword_html");
-						echo "<span class=\"extract\">";
-						if(preg_match_all("/((\s\S*){0,3})($keyword|$keyword_html)((\s?\S*){0,3})/i", $auszug, $match, PREG_SET_ORDER)); {
-							if(!$limit_extracts)
-								$number=count($match);
-							else
-								$number=$limit_extracts;
-							for ($h=0;$h<$number;$h++) { // Kein Limit angegeben also alle Vorkommen ausgeben
-								if (!empty($match[$h][3]))
-									printf("<i><b>..</b> %s<b>%s</b>%s <b>..</b></i>", $match[$h][1], $match[$h][3], $match[$h][4]);
-								}
-							}
-						echo "</span><br><br>";
-						flush();
-						}
-					fclose($fd);
-					}
-				}
-	  		@closedir($handle);
-			}
-		}
-	}
+        if($action == "SEARCH") {
+            foreach($s_dirs as $dir) {
+                $handle = @opendir($my_root.$dir);
+                while ($file = @readdir($handle)) {
+                    if (in_array($file, $s_skip)) { continue; }
+                    elseif ($count_hits >= $limit) { break; }
+                    elseif(is_dir($my_root.$dir."/".$file)) {
+                        $s_dirs = array("$dir/$file");
+                        search_dir($my_server, $my_root, $s_dirs, $s_files, $s_skip, $message_1, $message_2, $no_title, $limit_extracts, $byte_size, $HTTP_GET_VARS); // search_dir() rekursiv auf alle Unterverzeichnisse aufrufen
+                    } elseif (preg_match("/($s_files)$/i", $file)) {
+                    // Alle Dateien gemaess Endungen $s_files
+                    $fd = fopen($my_root.$dir."/".$file,"r");
+                    $text = fread($fd, $byte_size); // 50 KB
+                    $keyword_html = htmlentities($keyword);
+                    if ($case) {
+                        // Gross-/Kleinschreibung beruecksichtigen?
+                        $do=strstr($text, $keyword)||strstr($text, $keyword_html);
+                    } else {
+                        $do=stristr($text, $keyword)||stristr($text, $keyword_html);
+                    }
+                    if ($do) {
+                        $count_hits++;
+                        // Treffer zaehlen
+                        if(preg_match_all("=<title[^>]*>(.*)</title>=siU", $text, $titel)) {
+                        // Generierung des Link-Textets aus <title>...</title>
+                            if(!$titel[1][0]) // <title></title> ist leer...
+                                $link_title=$no_title; // ...also $no_title
+                            else $link_title=$titel[1][0];  // <title>...</title> vorhanden...
+                        } else {
+                            $link_title=$no_title; // ...ansonsten $no_title
+                        }
+                        $page = explode(".",$file);
+                        echo "<a href=\"/code/excitecms?page=$page[0]\" target=\"_self\" class=\"result\">$count_hits.  $link_title</a><br>"; // Ausgabe des Links
+                        $auszug = strip_tags($text);
+                        $keyword = preg_quote($keyword); // unescapen
+                        $keyword = str_replace("/","\/","$keyword");
+                        $keyword_html = preg_quote($keyword_html); // unescapen
+                        $keyword_html = str_replace("/","\/","$keyword_html");
+                        echo "<span class=\"extract\">";
+                        if (preg_match_all("/((\s\S*){0,3})($keyword|$keyword_html)((\s?\S*){0,3})/i", $auszug, $match, PREG_SET_ORDER));
+                        {
+                            if (!$limit_extracts)
+                                $number = count($match);
+                            else
+                                $number = $limit_extracts;
+                            for ($h = 0; $h<$number; $h++) {
+                            // Kein Limit angegeben also alle Vorkommen ausgeben
+                                if (!empty($match[$h][3]))
+                                    printf("<i><b>..</b> %s<b>%s</b>%s <b>..</b></i>", $match[$h][1], $match[$h][3], $match[$h][4]);
+                                }
+                        }
+                        echo "</span><br><br>";
+                        flush();
+                        }
+                    fclose($fd);
+                    }
+                }
+            @closedir($handle);
+            }
+        }
+    }
 
 
-// search_no_hits(): Ausgabe 'keine Treffer' bei der Suche
-function search_no_hits($HTTP_GET_VARS, $count_hits, $message_4) {
-	@$action=$HTTP_GET_VARS['action'];
-	if($action == "SEARCH" && $count_hits<1) // Volltextsuche, kein Treffer
-		echo "<p class=\"result\">$message_4</p>";
-	}
+    // search_no_hits(): Ausgabe 'keine Treffer' bei der Suche
+    function search_no_hits($HTTP_GET_VARS, $count_hits, $message_4)
+    {
+        @$action=$HTTP_GET_VARS['action'];
+        if ($action == "SEARCH" && $count_hits<1) // Volltextsuche, kein Treffer
+        echo "<p class=\"result\">$message_4</p>";
+    }
 
+// Template
 ?><!doctype html>
 <html>
 <head>
 <title>Search MCBVI Website</title>
 </head>
 <body>
-
 <?
 // search_form(): Gibt das Suchformular aus
-search_form($HTTP_GET_VARS, $limit_hits, $default_val, $message_5, $message_6, $PHP_SELF);
+    search_form($HTTP_GET_VARS, $limit_hits, $default_val, $message_5, $message_6, $PHP_SELF);
 // search_headline(): Ueberschrift Suchergebnisse
-search_headline($HTTP_GET_VARS, $message_3);
+    search_headline($HTTP_GET_VARS, $message_3);
 // search_error(): Auf Fehler testen und Suchfehler anzeigen
-search_error($HTTP_GET_VARS, $min_chars, $max_chars, $message_1, $message_2, $limit_hits);
+    search_error($HTTP_GET_VARS, $min_chars, $max_chars, $message_1, $message_2, $limit_hits);
 // search_dir(): Volltextsuche in Verzeichnissen (siehe config.php4)
-search_dir($my_server, $my_root, $s_dirs, $s_files, $s_skip, $message_1, $message_2, $no_title, $limit_extracts, $byte_size, $HTTP_GET_VARS);
+    search_dir($my_server, $my_root, $s_dirs, $s_files, $s_skip, $message_1, $message_2, $no_title, $limit_extracts, $byte_size, $HTTP_GET_VARS);
 // search_no_hits(): Ausgabe 'keine Treffer' bei der Suche
-search_no_hits($HTTP_GET_VARS, $count_hits, $message_4);
+    search_no_hits($HTTP_GET_VARS, $count_hits, $message_4);
 ?>
 </body>
 </html>
